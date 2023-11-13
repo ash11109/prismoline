97db593b9e09f3722041fdd978eb2a2539dc99db
ash11109 <ayushcool1275@gmail.com>
2023-11-13 16:16:05 +0530

    13-11-23 5

---
+++ src/components/dataVar.js
@@ -0,0 +1,34 @@
+export const proImgUrl = 'assets/img/product/';
+
+export const THERMOPLASTICPAINT = [
+    { id: 1, name: 'Snowlite', img: 'THERMO PLASTIC PAINT/SNOWLITE WHITE.jpg' },
+    { id: 2, name: 'Prismoline', img: 'THERMO PLASTIC PAINT/PRESMOLINE.jpg' },
+    { id: 3, name: 'Prismoline Premium', img: 'THERMO PLASTIC PAINT/PRESMOLINE PREMIUM.jpg' },
+    { id: 4, name: 'Kerb Marking Paint', img: 'THERMO PLASTIC PAINT/Karb marking.png' }
+];
+
+export const OTHER = [
+    { id: 1, name: 'Primer', img: 'OTHER/p2.jpg' }
+];
+
+export const GLASSBEAD = [
+    { id: 1, name: 'Prismolite Glass Bead', img: 'GLASS BEAD/Prismolite glass bead.png' },
+    { id: 2, name: 'Sovitech Glass Bead', img: 'GLASS BEAD/sovitech glass bead.png' },
+    { id: 3, name: 'Potter Thai Glass Bead', img: 'GLASS BEAD/potter thai.png' }
+];
+
+export const THTEEMPRODUCT = [
+    { id: 1, name: 'RED ILC', img: '3M PRODUCT/galvanised-w-beam-metal-crash-barrier-for-highway.jpg' },
+    { id: 2, name: 'YELLOW ILC', img: '3M PRODUCT/plastic-speed-breaker-removebg-preview.png' },
+    { id: 3, name: 'WHITE ILC', img: '3M PRODUCT/Road-Studs-removebg-preview.png' },
+    { id: 4, name: 'W/R ILC', img: '3M PRODUCT/Road-Studs.jpg' },
+    { id: 5, name: 'RED MOLDED', img: '3M PRODUCT/MOLDED SHANK FINAL.jpg' },
+    { id: 6, name: 'YELLOW MOLDED', img: '3M PRODUCT/MOLDED SHANK FINAL.jpg' },
+    { id: 7, name: 'WHITE MOLDED', img: '3M PRODUCT/MOLDED SHANK I.jpg' },
+    { id: 8, name: 'W/R MOLDED', img: '3M PRODUCT/MOLDED SHANK I.jpg' },
+    { id: 9, name: 'GREEN MOLDED', img: '3M PRODUCT/MOLDED SHANK.jpg' },
+    { id: 10, name: 'SAFETY CONE', img: '3M PRODUCT/TWIN MOLDED SHENK.webp' },
+    { id: 11, name: 'SPEED BREAKER', img: '3M PRODUCT/UTKARSH.jpg' },
+    { id: 12, name: 'HARDNER BOTLE', img: '3M PRODUCT/UTKARSH.jpg' },
+    { id: 13, name: 'RESIN EPOXY', img: '3M PRODUCT/UTKARSH.jpg' }
+];
